07fcaa558b7d61937553ea74d25d8a2e344762f1
Vitor Mattos <vitor.mattos@phprio.org>
2017-05-21 23:37:29 -0300

    Specify the path to drafter binary file

--- src/Oasis/Parser.php
+++ src/Oasis/Parser.php
@@ -26,9 +26,10 @@ class Parser
      * Parses the MSON input into an array.
      * @param  string $data   Must be valid MSON
      * @param  string $format Format to decode. Defaults to `json`
+     * @param  string $binary specify the location of the drafter binary
      * @return array          Array of MSON data
      */
-    public static function parse($data, $format = 'json')
+    public static function parse($data, $format = 'json', $binary = 'drafter')
     {
         if (!array_key_exists($format, self::$decoders)) {
             throw new Exception('Invalid decoder.');
@@ -36,7 +37,7 @@ class Parser
         $decoder = self::$decoders[$format];
 
         $input = file_put_contents('/tmp/input', $data);
-        $params = "drafter -f {$format} /tmp/input";
+        $params = "$binary -f {$format} /tmp/input";
 
         $results = shell_exec($params);
         return call_user_func([$decoder, 'decode'], $results);
